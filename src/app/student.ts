dcd96720dc1a3ae363f0e20ce4ef997a5103641b
Joyce Santiago <99730710+joyce-santiago@users.noreply.github.com>
2022-06-17 00:51:43 -0300

    Delete src directory

--- src/app/student.ts
+++
@@ -1,7 +0,0 @@
-export interface Student {
-    id: number;
-    nome: string;
-    curso: string;
-    semestre: number;
-    turno: string;
-}
--- src/app/teacher.ts
+++
@@ -1,5 +0,0 @@
-export interface Teacher {
-    id: number;
-    nome: string;
-    aulas: string;
-}
